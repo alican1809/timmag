46fc014247a328b091da131764933b6530604a3b
alican1809 <94050902+alican1809@users.noreply.github.com>
2022-09-25 16:06:50 +0200

    test 2inscription

---
+++ src/Form/RegisterType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class RegisterType extends AbstractType
+{
+   
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class,[
+                'label' => 'Prénom',
+                 'attr' => [
+                    'placeholder' => 'Saisir votre Prénom ici placeholder'
+                 ]
+            ])
+            ->add('lastname', TextType::class,[
+                'label' => 'Nom',
+                 'attr' => [
+                    'placeholder' => 'Saisir votre Nom ici placeholder'
+                 ]
+            ])
+            ->add('email', EmailType::class,[
+                'label' => 'Adresse mail',
+                 'attr' => [
+                    'placeholder' => 'Saisir votre adresse mail ici placeholder'
+                 ]
+            ])
+            ->add('password', RepeatedType::class,[
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => "Mot de passe",
+                    'attr' =>[
+                        'placeholder' => 'Saisir votre mode passe'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => "Cofimation de Mot de passe",
+                    'attr' =>[
+                        'placeholder' => 'Confirmer votre mode passe'
+                    ]
+                ]
+            ])
+            
+            ->add('submit', SubmitType::class,[
+                'label' => 'S\'inscrire',
+            ])
+
+            
+        ;
+    }
+}
