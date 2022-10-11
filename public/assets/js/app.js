965a71552b9cf02359654669d8dd01fa9cd5fc8c
alican1809 <94050902+alican1809@users.noreply.github.com>
2022-10-11 20:23:26 +0200

    animation frontk

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -8,68 +8,60 @@ function showTime() {
 
 showTime();
 
+
+
+
+
+
 var imgTim = document.querySelectorAll(".imgTim");
 console.log(imgTim)
 
 var nbrImgTim = imgTim.length;
 console.log(nbrImgTim)
 
-var aherfTim= document.querySelectorAll(".aherfTim");
+var aherfTim = document.querySelectorAll(".aherfTim");
 console.log(aherfTim)
 
-var animTimFly = document.querySelector(".animTimFly"); 
+var animTimFly = document.querySelector(".animTimFly");
 console.log(animTimFly)
 
-var max= 200;
-var min= 125;
-var maxTop= 0;
-var minTop= 50;
-var nbrProduct=aherfTim.length;
-if (nbrProduct>15){
-  nbrProduct=10
+var max = 200;
+var min = 125;
+var maxTop = 0;
+var minTop = 50;
+
+
+var nbrProduct = aherfTim.length;
+if (nbrProduct > 15) {
+  nbrProduct = 10
 }
-  for (let i = 0; i <nbrProduct; i++) { 
+
+
+for (let i = 0; i < nbrProduct; i++) {
   var nbrRandomHeight = Math.floor(Math.random() * (max - min) + min) + "px";
   var nbrRandomTop = Math.floor(Math.random() * (maxTop - minTop) + minTop) + "%";
 
   var animImgTim = document.createElement("img");
-  animImgTim.style.height = nbrRandomHeight ;
-  animImgTim.style.top = nbrRandomTop ;
+  animImgTim.style.height = nbrRandomHeight;
+  animImgTim.style.top = nbrRandomTop;
 
   var srcImgTim = imgTim[i].getAttribute("src");
-   
+
   animImgTim.setAttribute("src", srcImgTim);
   animImgTim.setAttribute("class", "animImgTim");
-  animImgTim.style.animationDuration = Math.random() * 10 + 8 + 's'; 
+  animImgTim.style.animationDuration = Math.random() * 10 + 8 + 's';
   console.log(animImgTim)
 
   var animAhrefTim = document.createElement("a");
   var aTim = aherfTim[i].getAttribute("href");
-  
+
   animAhrefTim.setAttribute("href", aTim);
   animAhrefTim.setAttribute("class", "animAhrefTim");
   console.log(animAhrefTim)
-  
+
   animAhrefTim.appendChild(animImgTim);
   animTimFly.appendChild(animAhrefTim);
-  }
-  
-
-if (aherfTim.length) {
-  
 }
-
-  
-
-
-// 
-// tim.setAttribute('src','');
-
-
-// 
-// tim.style.width = ;
-// 
-
 
 
 
