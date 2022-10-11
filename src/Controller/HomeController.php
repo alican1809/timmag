055eded874ea31444ff4f6c4e6e1ddece233b657
alican1809 <94050902+alican1809@users.noreply.github.com>
2022-10-11 16:38:24 +0200

    animation front

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,8 +3,9 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Category;
 use App\Repository\ProductRepository;
-
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,16 +14,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository ,CategoryRepository $categoryRepository): Response
     {
 
         $products = $productRepository->findAll();
-
+        $category = $categoryRepository->findAll();
+      
         return $this->render('home/index.html.twig', [
             "product1"=>$products[0],
             "product2"=>$products[2],
             "product3"=>$products[3],
-            "products"=>$products
+            "products"=>$products,
+            
         ]);
     }
 
